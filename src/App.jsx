e8990f388b096f3ea91db126ab47e170ba3e227a
DJones <dwaynejones@pursuit.com>
2024-03-07 11:32:19 -0500

    session start, session end, start recording, stop recording set up and functional

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import New from "./Pages/New";
 import Show from "./Pages/Show";
 import FourOFour from "./Pages/FourOFour";
 import VideoSession from "./Components/VideoSession";
-import Video from "./Components/Video";
 
 function App() {
   
@@ -19,7 +18,6 @@ function App() {
         <Router>
           <Routes>
           <Route path="/video-session" element={<VideoSession />} />
-          <Route path="/test" element={<Video />} />
             <Route path="/" element={<Home />} />
             {/* <Route path="/videos" element={<Index />} /> */}
             <Route path="/videos/:id" element={<Show />} />
