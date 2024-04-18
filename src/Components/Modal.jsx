8ca3c2371ff5261848ac6a4ddb6ecb1205f3031c
Dwayne <dwaynejones@pursuit.org>
2024-04-17 23:06:59 -0400

    recieved the signed urls difficulty rendering logic is sound retun has issues

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import "./Modal.css";
 
-export default class Modal extends React.Component {
-  onClose = (e) => {
-    this.props.onClose && this.props.onClose(e);
-  };
-  render() {
-    if (!this.props.show) {
-      return null;
-    }
-    return (
-      <div class="modal" id="modal">
-        <h2>Modal Window</h2>
-        <div class="content">{this.props.children}</div>
-        <div class="actions">
-          <button class="toggle-button" onClick={this.onClose}>
-            close
-          </button>
-        </div>
-      </div>
-    );
+export default function Modal({ show, onClose, children, selectedVideo }) {
+  if (!show) {
+    return null;
   }
+  return (
+    <div className="modal" id="modal">
+      <h2>Modal Window</h2>
+      <div className="content">
+        {React.cloneElement(children, { selectedVideo })}
+      </div>
+      <div className="actions">
+        <button className="toggle-button" onClick={onClose}>
+          close
+        </button>
+      </div>
+    </div>
+  );
 }
